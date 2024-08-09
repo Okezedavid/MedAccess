46280690b08bb26c85144a9da7a0b1fcff878c7f
OkezeDavid <davidugochukwu080@gmail.com>
2024-08-09 22:27:22 +0100

    Added some changes to my account component

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -4,7 +4,7 @@ import { FaSignOutAlt } from 'react-icons/fa';
 import Link from 'next/link';
 import Image from 'next/image';
 import { auth } from '../../firebaseConfig';
-import profile from '../../assets/imgs/profile.svg';
+
 import Navbar from "../Dashboard/page"
 
 interface User {
@@ -34,40 +34,40 @@ export default function Account() {
   return (
     <div> 
       <nav>
-             <Navbar />
+        <Navbar />
       </nav>
-    <div className="container mx-auto ">
-      <div className="flex flex-col md:flex-row items-center p-6 md:p-0 space-y-4 md:space-y-0">
-        <div className="hidden md:flex flex-col items-center justify-center w-full md:w-2/4 space-y-4 h-screen -mt-42">
-          <Image src={profile} alt="Profile" className="w-full h-auto relative" width={1920} height={1980} />
-        </div>
-        <div className="flex flex-col items-center justify-center w-full md:w-1/2 space-y-4 h-screen p-6 md:p-0 relative -top-8">
-          <div className="text-center space-y-2">
+      <div className="container mx-auto p-4 flex flex-col items-center justify-center h-screen relative -top-28">
+        <div className="bg-blue-900 text-white rounded-lg shadow-lg p-8 w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
+          <div className="flex flex-col items-center space-y-4">
             {user && (
               <>
-                <label htmlFor="name" className="text-base md:text-2xl font-semibold text-gray-700 font-sans">Name</label>
-                <h3 id="name" className="text-base md:text-xl py-2 font-medium text-gray-900 font-serif">{user.name}</h3>
-                <label htmlFor="email" className="text-base md:text-2xl font-semibold text-gray-700 relative -bottom-4 font-sans">Email</label>
-                <h3 id="email" className="text-base md:text-xl text-gray-600 py-5 font-serif">{user.email}</h3>
+                <h2 className="text-2xl font-bold font-sans">User Profile</h2>
+                <div className="text-left w-full space-y-2">
+                  <div>
+                    <label htmlFor="email" className="text-sm font-medium">Email</label>
+                    <h3 id="email" className="bg-white text-gray-600 rounded-md py-2 px-3 text-xs md:text-sm">{user.email}</h3>
+                  </div>
+                  <div>
+                    <label htmlFor="name" className="text-sm font-medium">Username</label>
+                    <h3 id="name" className="bg-white text-gray-600 rounded-md py-2 px-3 text-xs md:text-sm">{user.name}</h3>
+                  </div>
+                </div>
               </>
             )}
           </div>
-          <div className="flex flex-col items-center space-y-4">
-          <Link href="/resetpassword">
-          <button className="px-4 py-2 text-sm font-sans md:text-base bg-blue-500 text-white rounded-xl shadow-md hover:bg-blue-600">
-              Change Password
-            </button></Link>
-            <Link href="/signin">
-              <button className="flex items-center text-sm md:text-base px-16 py-2 bg-red-500 text-white rounded-xl shadow-md hover:bg-red-600">
+          <div className="mt-6 flex flex-col space-y-3 sm:space-y-0 sm:flex-row sm:space-x-4">
+            <Link href="/resetpassword" className="w-full">
+              <button className="font-serif w-full py-2 mb-3 sm:mb-0 bg-blue-700 text-white rounded-lg hover:bg-blue-600 text-sm md:text-base">Change Password</button>
+            </Link>
+            <Link href="/signin" className="w-full">
+              <button className="font-serif w-full py-2 bg-gray-900 text-white rounded-lg flex items-center justify-center hover:bg-red-600 text-sm md:text-base">
                 <FaSignOutAlt className="mr-2" />
-                Sign out
+                Logout
               </button>
             </Link>
           </div>
         </div>
       </div>
     </div>
-    </div>
   );
 }
-     
